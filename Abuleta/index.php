b7cd754782830fa3fd7e7b658890e1681d089d06
autorun16 <ariefrahmad9@gmail.com>
2022-12-20 15:03:54 +0700

    Login dan Regis
    fungsi login dan regis sudah berfungsi

--- Abuleta/index.php
+++ Abuleta/index.php
@@ -1,11 +1,11 @@
-<!-- <?php
-        // session_start();
-
-        // if (!isset($_SESSION["login"])) {
-        //     header("Location: ./pages/login.php");
-        //     exit;
-        // }
-        ?> -->
+<?php
+    session_start();
+    
+    if (!isset($_SESSION["login"])) {
+        header("Location: ./pages/login.php");
+        exit;
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
